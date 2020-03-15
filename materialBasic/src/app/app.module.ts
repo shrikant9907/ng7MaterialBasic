192e98574276ed63cf6424c241d493c933ef9883
Shrikant Dev <shrikant9907@gmail.com>
2020-03-15 15:12:09 +0530

    home and profile page

--- materialBasic/src/app/app.module.ts
+++ materialBasic/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
